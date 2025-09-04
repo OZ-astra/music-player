c0fb8669d2fe06022336bcbab7b70863e672c225
Ozioma Ekweh <oziomaekweh@gmail.com>
2025-09-04 19:03:47 +0200

    set data

--- src/e-commerce/App.jsx
+++ src/e-commerce/App.jsx
@@ -1,3 +1,33 @@
+import largeImages from "./data.js";
+
 export default function Ecommerce() {
-  return <div>i am a div</div>;
+  return (
+    <div>
+      <header className="border border-red-500">
+        <div className=" bg-[gold] rounded-b-2xl flex justify-between px-4 pt-5 pb-2 ">
+          <img
+            src="./commerceAssets/logo.jpg"
+            alt="logo"
+            className="w-12 h-12 rounded-[50%]"
+          />
+          <button>
+            <img
+              src="./commerceAssets/icons/menu.svg"
+              alt="menu icon"
+              className="w-10"
+            />
+          </button>
+        </div>
+        <div>
+          {largeImages.map((image, index) => {
+            return (
+              <div>
+                <img src={image} alt="" />
+              </div>
+            );
+          })}
+        </div>
+      </header>
+    </div>
+  );
 }
---
+++ src/e-commerce/data.js
@@ -0,0 +1,35 @@
+const largeImages = [
+  "./commerceAssets/bags/hangedBags-1.jpg",
+  "./commerceAssets/kitchen/juicer-1.jpg",
+  "./commerceAssets/kitchen/groupedStanleyCup.jpg",
+  "./commerceAssets/footwear/brownCrossedHeel.jpg",
+];
+
+const data = [
+  {
+    bags: [
+      "./commerceAssets/bags/bags-2.jpg",
+      "./commerceAssets/bags/hangedBags-1.jpg",
+      "./commerceAssets/bags/kidsBags.jpg",
+    ],
+  },
+  {
+    shoes: [
+      "./commerceAssets/footwear/blackHeel.jpg",
+      "./commerceAssets/footwear/brownCrossedHeel.jpg",
+      "./commerceAssets/footwear/shoe-1.jpg",
+      "./commerceAssets/footwear/greenHeel.jpg",
+    ],
+  },
+  {
+    kitchen: [
+      "./commerceAssets/kitchen/blender-1.jpg",
+      "./commerceAssets/kitchen/purplepot.jpg",
+      "./commerceAssets/kitchen/oneStanleyCup.jpg",
+      "./commerceAssets/kitchen/brush.jpg",
+      "./commerceAssets/kitchen/woodenUtensil.jpg",
+    ],
+  },
+];
+
+export default largeImages;
